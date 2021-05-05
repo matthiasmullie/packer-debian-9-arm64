128d4bb5e5037ab09b11feb590b8bf167e26d930
Matthias Mullie <git@mullie.eu>
2021-05-05 21:49:26 +0200

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+Builds a debian9 arm64 Vagrant box.
+
+Requires packer, vagrant, parallels & parallels-virtualization-sdk.
+
+Run:
+
+    packer build debian-9.13-arm64.json
+
+99% this code was stolen from either lavabit/robox or chef/bento.
---
+++ scripts/common/zerodisk.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -ux
+
+# Whiteout root
+rootcount=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
+rootcount=$(($rootcount-1))
+dd if=/dev/zero of=/zerofill bs=1K count=$rootcount || echo "dd exit code $? suppressed"
+rm --force /zerofill
+
+# Whiteout boot if the block count is different then root, otherwise if the
+# block counts are identical, we assume both folders are on the same parition
+bootcount=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
+bootcount=$(($bootcount-1))
+if [ $rootcount != $bootcount ]; then
+  dd if=/dev/zero of=/boot/zerofill bs=1K count=$bootcount || echo "dd exit code $? suppressed"
+  rm --force /boot/zerofill
+fi
+
+# If blkid is installed it to locate the swap partition
+if [ -f '/sbin/blkid' ]; then
+  swapuuid="`/sbin/blkid -o value -l -s UUID -t TYPE=swap`"
+else
+  swapuuid=""
+fi
+
+# Whiteout the swap partition
+if [ "x${swapuuid}" != "x" ]; then
+  swappart="`readlink -f /dev/disk/by-uuid/$swapuuid`"
+  /sbin/swapoff "$swappart"
+  dd if=/dev/zero of="$swappart" bs=1K || echo "dd exit code $? suppressed"
+  /sbin/mkswap -U "$swapuuid" "$swappart"
+fi
+
+# Sync to ensure that the delete completes before we move to the shutdown phase.
+sync
+sync
+sync
+
+echo "All done."
+exit 0
